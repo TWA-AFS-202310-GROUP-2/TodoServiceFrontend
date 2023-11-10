855e3baed722da1db4bdcca79958f20122d97b3e
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 14:55:59 +0800

    feat: complete todo-detail mock view

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
 
 @Component({
   selector: 'app-todo-detail',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-detail.component.css']
 })
 export class TodoDetailComponent {
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+    }
+
+    getTodoDetail:ToDoItem = {
+      id: 1,
+      title: 'First Item',
+      description: 'This is the first item',
+      isDone: false,
+    }; 
 
 }
