f538fdaa4f3856792a7dd7f470b82e11de03fdc6
Zliu53 <ZLiu53@slb.com>
2023-11-10 13:10:27 +0800

    creata todo component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,18 +4,21 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
- items:ToDoItem[]=[{
-  title:'first',
-  id:1,
-  description: 'eat lunch',
-  isDone: false
- },{
-  title:'second',
-  id:2,
-  description: 'eat dinner',
-  isDone: false
- }];
+  items: ToDoItem[] = [
+    {
+      title: 'first',
+      id: 1,
+      description: 'eat lunch',
+      isDone: false,
+    },
+    {
+      title: 'second',
+      id: 2,
+      description: 'eat dinner',
+      isDone: false,
+    },
+  ];
 }
