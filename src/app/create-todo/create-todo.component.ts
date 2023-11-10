924c508e6accc2d71d06c5c0d48b1de75051ad00
Shang Huang <shuang19@slb.com>
2023-11-10 16:39:21 +0800

    feat: add a create method in todo http service

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { TodoService } from '../service/todo.service';
+import { TodoHttpService } from '../service/todo-http.service';
 
 @Component({
   selector: 'app-create-todo',
@@ -10,7 +11,10 @@ import { TodoService } from '../service/todo.service';
 export class CreateTodoComponent {
   constructor(
     private formBuilder: FormBuilder,
-    private todoService: TodoService){}
+    private todoService: TodoService,
+    private todoHttpService: TodoHttpService
+    ){}
+    
   todoForm = this.formBuilder.group(
     {
       title: '',
@@ -25,8 +29,10 @@ export class CreateTodoComponent {
     const formValues = this.todoForm.value
     if (formValues.title && formValues.description)
     {
-      this.todoService.create(formValues.title,formValues.description)
+      //this.todoService.create(formValues.title,formValues.description)
+      this.todoHttpService.create(formValues.title, 
+        formValues.description).subscribe(()=>{this.todoForm.reset()})
     }
-    this.todoForm.reset()
+    //this.todoForm.reset()
   }
 }
