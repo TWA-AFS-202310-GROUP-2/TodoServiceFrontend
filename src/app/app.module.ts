feadd15967935f8f16c2c61491122c2e56c11c1e
Yaxin Yang <yyang66@slb.com>
2023-11-10 13:22:40 +0800

    feat: add reactive form and console.log add content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { CreateTodolistComponent } from './create-todolist/create-todolist.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CreateTodolistComponent } from './create-todolist/create-todolist.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
