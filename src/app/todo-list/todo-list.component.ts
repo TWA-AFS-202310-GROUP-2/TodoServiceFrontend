d318fa31bf68e4a8056f07f05307842070521566
Zliu53 <ZLiu53@slb.com>
2023-11-10 15:19:52 +0800

    Feat: Add more detail routing

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
 import { TodoService } from '../service/todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,11 +10,15 @@ import { TodoService } from '../service/todo.service';
 })
 export class TodoListComponent {
   items: ToDoItem[] = [];
-  constructor(private _todoService: TodoService) {}
+  constructor(private _todoService: TodoService, private router: Router) {}
+
   ngOnInit() {
     this.items = this._todoService.getAll();
   }
   onMarkDone(id: number) {
     this._todoService.markDone(id);
+  }
+  onGoToDetail(id: number) {
+    this.router.navigateByUrl(`detail/${id}`);
   }
 }
