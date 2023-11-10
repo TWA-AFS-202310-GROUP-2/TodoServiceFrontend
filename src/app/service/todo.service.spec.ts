cc31c2450467a1e4b301250e02ba5b5bc418bfa4
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 14:30:42 +0800

    fix: test expect value

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -14,19 +14,7 @@ describe('TodoService', () => {
         title: 'First Item',
         description: 'This is the first item',
         isDone: false,
-      },
-      {
-        id: 2,
-        title: 'Second Item',
-        description: 'This is the second item',
-        isDone: true,
-      },
-      {
-        id: 3,
-        title: 'Third Item',
-        description: 'This is the third item',
-        isDone: false,
-      },
+      }
     ];
   });
 
@@ -35,16 +23,44 @@ describe('TodoService', () => {
   });
 
   it('should return all items when getAll', () => {
-    expect(service.getAll().length).toBe(3);
+    const items = service.getAll();
+    expect(items).toEqual(
+      [
+        {
+          id: 1,
+          title: 'First Item',
+          description: 'This is the first item',
+          isDone: false,
+        }
+      ]
+    )
   });
 
   it('should create a new item when create', () => {
-    service.create('New Item', 'This is a new item');
-    expect(service.items.length).toBe(4);
+    service.create('Second Item', 'This is the second item');
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'First Item',
+        description: 'This is the first item',
+        isDone: false,
+      },
+      {
+        id: 2,
+        title: 'Second Item',
+        description: 'This is the second item',
+        isDone: false,
+      }]);
   });
 
   it('should mark an item as done when markAsDone', () => {
     service.markAsDone(1);
-    expect(service.items[0].isDone).toBe(true);
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'First Item',
+        description: 'This is the first item',
+        isDone: true,
+      }]);
   });
 });
