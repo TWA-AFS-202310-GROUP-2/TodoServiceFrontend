755ebaf55d4d7330e2a3a48c2f1ab55980e3afca
Yaxin Yang <yyang66@slb.com>
2023-11-10 14:04:55 +0800

    feat: use markdone method in todoitem component and change the color of button's disabled property

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -8,10 +8,14 @@ import { TodoHttpService } from '../service/todo-http.service';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent {
+
   items: ToDoItem[] =[]
 
   constructor(private todoHttpService :TodoHttpService) {
 
+  }
+  onMarkDone(id: number) {
+    this.todoHttpService.markDone(id)
   }
   ngOnInit() {
     this.items = this.todoHttpService.getAll()
