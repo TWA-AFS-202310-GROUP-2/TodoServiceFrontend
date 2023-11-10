c90aeb929d43ced9e7827422e25555913bec6be8
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 13:10:49 +0800

    feat: add create-todo component

---
+++ src/app/create-todo/create-todo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-todo',
+  templateUrl: './create-todo.component.html',
+  styleUrls: ['./create-todo.component.css'],
+})
+export class CreateTodoComponent {}
