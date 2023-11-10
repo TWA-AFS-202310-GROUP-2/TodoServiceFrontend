6df4deb16b9c077037d26fef1294bb1052ee8283
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 13:05:40 +0800

    style: code format

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,7 +4,7 @@ import { ToDoItem } from 'src/model/ToDoItem';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   items: ToDoItem[] = [
@@ -12,19 +12,19 @@ export class TodoListComponent {
       id: 1,
       title: 'First Item',
       description: 'This is the first item',
-      isDone: false
+      isDone: false,
     },
     {
       id: 2,
       title: 'Second Item',
       description: 'This is the second item',
-      isDone: false
+      isDone: false,
     },
     {
       id: 3,
       title: 'Third Item',
       description: 'This is the third item',
-      isDone: false
-    }
+      isDone: false,
+    },
   ];
 }
