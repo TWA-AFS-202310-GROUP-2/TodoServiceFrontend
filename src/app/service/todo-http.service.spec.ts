d9227badf833f279c56b51f9ac77235db0f347dc
Yaxin Yang <yyang66@slb.com>
2023-11-10 14:21:45 +0800

    test: add getall service test

--- src/app/service/todo-http.service.spec.ts
+++ src/app/service/todo-http.service.spec.ts
@@ -8,9 +8,29 @@ describe('TodoHttpService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoHttpService);
+    service.items = [
+      {
+        id: 1,
+        title: "monday",
+        description: "bad day",
+        isDone: false,
+      }
+    ]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when getAll', ()=>{
+    const items = service.getAll()
+    expect(items).toEqual([
+      {
+        id: 1,
+        title: "monday",
+        description: "bad day",
+        isDone: false,
+      }
+    ])
   });
 });
