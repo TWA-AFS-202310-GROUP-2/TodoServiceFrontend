9b024ee61888745249d41f8d412d8782145ca4ce
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 14:21:13 +0800

    test: add todo service tests

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,43 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'First Item',
+        description: 'This is the first item',
+        isDone: false,
+      },
+      {
+        id: 2,
+        title: 'Second Item',
+        description: 'This is the second item',
+        isDone: true,
+      },
+      {
+        id: 3,
+        title: 'Third Item',
+        description: 'This is the third item',
+        isDone: false,
+      },
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all items when getAll', () => {
+    expect(service.getAll().length).toBe(3);
+  });
+
+  it('should create a new item when create', () => {
+    service.create('New Item', 'This is a new item');
+    expect(service.items.length).toBe(4);
+  });
+
+  it('should mark an item as done when markAsDone', () => {
+    service.markAsDone(1);
+    expect(service.items[0].isDone).toBe(true);
   });
 });
