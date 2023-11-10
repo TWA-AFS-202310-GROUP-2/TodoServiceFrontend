38f8846f31d87388f0fa0aa737e3f1377ae7e5f3
Shang Huang <shuang19@slb.com>
2023-11-10 14:31:25 +0800

    test: add tests for todo service methods

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,48 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items=[{
+      id: 1,
+      title: 'buy milk',
+      description: 'this morning',
+      isDone: false
+    }]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get all items when call getAll', () => {
+    //given
+    //when
+    const items = service.getAll()
+    //then
+    expect(items).toEqual(
+      [{id: 1,
+      title: 'buy milk',
+      description: 'this morning',
+      isDone: false}]
+    )
+  })
+
+  it('should create a new item when call create', () => {
+    service.create('new item', 'new item description')
+    expect(service.items).toEqual(
+      [{id: 1,
+      title: 'buy milk',
+      description: 'this morning',
+      isDone: false},
+      {
+        id: 2,
+        title: 'new item',
+        description: 'new item description',
+        isDone: false
+      }])
+  })
+  
+  it('should mark item as done when call markDone', () => {
+    service.markDone(1)
+    expect(service.items[0].isDone).toEqual(true)
+  })
 });
