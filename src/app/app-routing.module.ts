8a8d7135b77944fbad6defbd3848c84a33186df7
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-10 14:46:00 +0800

    feat: add routing and todoDetail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component'; // Add this
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todo-list',
+    component: TodoListComponent
+  },
+  {
+    path: 'todo-detail/:id',
+    component: TodoDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: '/todo-list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
